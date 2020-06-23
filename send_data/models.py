ee2c465c48913b59a682f8283e64a4df48646503
dhruv-shindhe <dhruv.shindhe@gmail.com>
2020-06-23 15:46:09 +0530

    amplitude and time added

--- send_data/models.py
+++ send_data/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class data_store(models.Model):
-    value = models.IntegerField()
+    amplitude = models.FloatField(null=True)
+    time = models.FloatField(null=True)
